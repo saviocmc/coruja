a340afb3c4bb4b97c6db9052e840ad9fd1cfc237
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2017-02-25 19:15:51 -0300

    Using the PIR library in the main.cpp For now, the callback function just prints to the Serial port when a motion is detected.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,19 @@
 #include <Arduino.h>
+#include <PIR.h>
 
-// Put the pin number of all PIR sensors connected
-// to the arduino on the array below
-// It is used in the checkPir() function
-const int PIR_PINS[] = {1,2,3,4};
-int PIR_SIZE;
+PIR pir(2000);
 
-void setup(){
+void onMotionDetected(char* identifier, int pin) {
+	Serial.print("Moviment on: ");
+	Serial.println(identifier);
+}
+
+void setup() {
 	Serial.begin(9600);
-	PIR_SIZE = sizeof(PIR_PINS)/sizeof(PIR_PINS[0]);
+	pir.add("Back door", 8, onMotionDetected);
+	pir.add("Garage", 9, onMotionDetected);
 }
 
-bool checkPIR(){
-	// TODO find a way more efficient to do this
-	// Instead of the for, I could generate masks with the values in the array
-	// and use the mask in the registers of arduino checking all PIR sensors at once
-	for(int i = 0; i<PIR_SIZE; i++){
-		if(digitalRead(PIR_PINS[i]))
-			return true;
-	}
-	return false;
-}
-
-void loop(){
-	if(checkPIR()){
-		// Motion detected! Do something!
-	}
+void loop() {
+	pir.loop();
 }
