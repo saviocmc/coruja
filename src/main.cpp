18984d67584e55b33ba1833750e33fb129470a50
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2017-03-07 17:15:32 -0300

    Using the LASER library in the main program. The library is used to drive a laser movement sensor built into the main board.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,33 @@
 #include <Arduino.h>
 #include <PIR.h>
+#include <LASER.h>
 
-PIR pir;
+LASER* laser;
+PIR* pir;
 
-void onMotionDetected(char* identifier, int pin) {
+void onPirMotionDetected(char* identifier, int pin) {
+	static int count;
 	Serial.print("Moviment on: ");
-	Serial.println(identifier);
+	Serial.print(identifier);
+	Serial.print("\t");
+	Serial.println(count++);
+}
+
+void onLaserMotionDetected() {
+	static int count;
+	Serial.print("Laser motion ");
+	Serial.println(count++);
 }
 
 void setup() {
 	Serial.begin(9600);
-	pir.add("Back door", 8, onMotionDetected);
-	pir.add("Garage", 9, onMotionDetected);
+	laser = new LASER(A0, 800, onLaserMotionDetected);
+	pir = new PIR(5000);
+	pir->add("Back door", 8, onPirMotionDetected);
+	pir->add("Garage", 9, onPirMotionDetected);
 }
 
 void loop() {
-	pir.loop();
+	pir->loop();
+	laser->loop();
 }
